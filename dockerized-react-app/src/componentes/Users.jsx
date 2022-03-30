37a0d38bf089eb168fb9de982a21bf9ce1af60a4
pH2O4 <ph2oalencar@gmail.com>
2022-03-30 02:57:53 -0300

    ---

--- dockerized-react-app/src/componentes/Users.jsx
+++ dockerized-react-app/src/componentes/Users.jsx
@@ -2,7 +2,7 @@ import { Component, React, useEffect, useState } from "react";
 import './Home.css'
 import './Users.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { } from '@fortawesome/free-solid-svg-icons'
+import {faUserGroup } from '@fortawesome/free-solid-svg-icons'
 import { Button } from "react-bootstrap";
 import Axios from "axios";
 
@@ -13,7 +13,7 @@ class Users extends Component {
     }
 
     async componentDidMount() {
-        const responseUsers = await Axios.get('https://gorest.co.in/public/v2/users',{
+        const responseUsers = await Axios.get('https://gorest.co.in/public/v2/users', {
             headers: {
                 Authorization: 'Bearer ' + localStorage.Token
             }
@@ -24,22 +24,33 @@ class Users extends Component {
 
 
     render() {
-     
+
 
         const { users } = this.state
-
+        console.log(users)
+        //using the same Home css
         return (
             <div className="Users">
-            <div className="Home">
-                <div className="ConsultUserInfomation">
-                    f
+                <div className="Home">
+                    <div className="ConsultUserInfomation">
+                        <div>
+
+                        </div>
+                    </div>
+                    <div className="Divisor">
+                    </div>
+                    <div className="AllPosts">
+                        <h1 className="TitleAllUsers" > <FontAwesomeIcon icon={faUserGroup } /> All Users</h1>
+                        <div className="AllUsersFromApi">
+                           {users.map(user => (
+                            <div key={user.id}>
+                                <p> <b>User id:</b> {user.id} <b>Name:</b> {user.name} <b>Email</b> {user.email}  </p>
+
+                            </div>))}   
+                        </div>
+                      
+                    </div>
                 </div>
-                <div className="Divisor">
-                </div>
-                <div className="AllPosts">
-                   f
-                </div>
-            </div>
             </div>
 
         )
