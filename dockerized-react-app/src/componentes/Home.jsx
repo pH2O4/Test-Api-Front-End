ce9fb145607cd15ba02e8a7e157df9cdb0774088
pH2O4 <ph2oalencar@gmail.com>
2022-03-30 23:50:09 -0300

    ---

--- dockerized-react-app/src/componentes/Home.jsx
+++ dockerized-react-app/src/componentes/Home.jsx
@@ -1,9 +1,10 @@
 import { Component, React, useEffect, useState } from "react";
 import './Home.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserGroup, faSignsPost, faComments, faMessage} from '@fortawesome/free-solid-svg-icons'
+import { faUserGroup, faSignsPost, faComments, faMessage } from '@fortawesome/free-solid-svg-icons'
 import { Button } from "react-bootstrap";
 import Axios from "axios";
+import { post } from "jquery";
 
 class Home extends Component {
 
@@ -13,29 +14,40 @@ class Home extends Component {
     }
 
     async componentDidMount() {
-        const responsePosts = await Axios.get('https://gorest.co.in/public/v2/posts',{
+        const responsePosts = await Axios.get('https://gorest.co.in/public/v2/posts', {
             headers: {
                 Authorization: 'Bearer ' + localStorage.Token
             }
         })
         this.setState({ posts: responsePosts.data })
 
-          const responseComents = await Axios.get('https://gorest.co.in/public/v2/comments',{
+        const responseComents = await Axios.get('https://gorest.co.in/public/v2/comments', {
             headers: {
                 Authorization: 'Bearer ' + localStorage.Token
             }
-          })
+        })
         this.setState({ comments: responseComents.data })
     }
 
 
     render() {
-        const ShowComentsEachDivComentsPosts = (e) => {
-           console.log(e)
+        const ShowPostComents = (postId) => { 
+            console.log(postId)
+            let div = document.getElementsByClassName(postId);
+            let TrueDiv = div[0]
+            if (TrueDiv.style.display === "none") {
+                TrueDiv.style.display = "block";
+            } else {
+                TrueDiv.style.display = "none";
+            }
         }
+        const PostComents = (postId) => {
+            console.log(postId)
+            Axios.post(`/public/v2/posts/${postId}/comments`, {
 
+            })
+        }
         const { posts, comments } = this.state
-
         return (
             <div className="Home">
                 <div className="ConsultUserInfomation">
@@ -56,19 +68,21 @@ class Home extends Component {
                                     <p> <b>User id: {post.user_id}</b>  </p>
                                     <p> <b>Título: {post.title}</b>  </p>
                                     <p> {post.body}  </p>
-                                    <div> <button onClick={() => ShowComentsEachDivComentsPosts()} className="ButtonsComents"><FontAwesomeIcon icon={faComments} /> Show Comments </button>
-                                    <button className="ButtonsComents"><FontAwesomeIcon icon={faMessage} /> Add Coments</button> </div> 
+                                    <div> <button onClick={() => ShowPostComents(post.id)} className="ButtonsComents"><FontAwesomeIcon icon={faComments} /> Show Comments </button>
+                                    
+                                        <button onClick={() => PostComents(post.id)} className="ButtonsComents"><FontAwesomeIcon icon={faMessage} /> Add Coments</button> </div>
                                     <div className={post.id} id="CommentsSpace">
 
-                                    {comments.map(coment => 
-                                    post.id == coment.post_id &&
-                                    <div key={coment.id}> 
-                                    <p> <b>{coment.name}:</b>  {coment.body} </p>
-                                    </div>
-                                    )}
+                                        {comments.map(coment =>
+                                            post.id == coment.post_id &&
+                                            <div key={coment.id}>
+                                                <p> <b>{coment.name}:</b>  {coment.body} </p>
+                                            </div>
+                                            
+                                        )}
                                     </div>
 
-                                       
+
                                 </div>
 
                             ))}
